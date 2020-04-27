19191bbdecfc88eeb59ddd1ed3426a56f77f6203
miguel-acuna-gutierrez <58172954+miguel-acuna-gutierrez@users.noreply.github.com>
2020-04-27 13:38:19 +0200

    Add files via upload
    adasdasdasdas

---
+++ datosPrueba.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+[{"0":"1","codigo":"1","1":"papas","descripcion":"papas","2":"34","precio":"34"},{"0":"2","codigo":"2","1":"manzanas","descripcion":"manzanas","2":"23.5","precio":"23.5"},{"0":"3","codigo":"3","1":"sandia","descripcion":"sandia","2":"31","precio":"31"},
+{"0":"4","codigo":"4","1":"melocoton","descripcion":"melocoton","2":"33","precio":"33"}]
+</body>
+</html>
